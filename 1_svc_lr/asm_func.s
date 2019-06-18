7c8d4ac3d2178a4f613ba6998b7c6f7daf6c853d
JEFF1216 <43406666+GOGOGOGOGOGOG@users.noreply.github.com>
2019-06-18 16:51:01 +0800

    Increase commitment of .s file

--- 1_svc_lr/asm_func.s
+++ 1_svc_lr/asm_func.s
@@ -39,5 +39,6 @@ sys_call:
 .type svc_handler, %function
 .global svc_handler
 svc_handler:
-	mov r0, lr
-	b svc_handler_c
+	mov r0, lr// 把exec_return給r0也就是handler
+	b svc_handler_c //回到svc_handler_c函式
+	
